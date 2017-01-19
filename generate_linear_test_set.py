ba8e122eaaf47acdd8933e7321a8f72f1d8b3df0
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-01-19 18:33:54 +0100

    Small modifications

--- generate_linear_test_set.py
+++ generate_linear_test_set.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
+import random
 
 number_max_pts=2000
 number_min_pts=20
 noise_to_signal_maxratio=0.9
+
+number_pairs=3000
+
+if __name__== '__main__':
+
+    for i in range(number_pairs):
 
